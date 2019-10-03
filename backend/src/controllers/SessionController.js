7d93daf86ddf65d02f805db22874eb431b91e601
GleidsonDaniel <gleidson10daniel@hotmail.com>
2019-10-03 16:22:33 -0300

    Added SessionControler

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,14 @@
+import User from '../models/User';
+
+const session = {
+	async store(req, res) {
+		const { email, password } = req.body;
+		let user = await User.findOne({ email, password });
+		if (!user) {
+			user = await User.create({ email, password });
+		}
+		return res.json(user);
+	},
+};
+
+export default session;
